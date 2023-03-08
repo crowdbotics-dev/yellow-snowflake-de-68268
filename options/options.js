2ff8931b23537401d35d02ca7865b87d12650aa6
Crowdbotics <engineering@crowdbotics.com>
2023-03-08 00:18:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-snowflake-de-68268",
+  url: "https://yellow-snowflake-de-68268.botics.co",
+  api: "https://yellow-snowflake-de-68268.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
